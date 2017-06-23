4ab90e3d6f138c3837fc0e8919bff2532be337f9
airnold <jsyoo@bistel.com>
2017-06-23 17:34:01 +0900

    chore : add event dispatcher

--- src/app/common/event/index.ts
+++ src/app/common/event/index.ts
@@ -1,4 +1,3 @@
+export { EventDispatcher } from './event-dispatcher';
 export { ChartEvent } from './chart-event.constant';
 export { IChartEvent, ChartEventData } from './chart-event.interface';
-
-
