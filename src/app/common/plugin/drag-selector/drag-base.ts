b7f99126c75de3b5f890632ab69a22f573ae3e27
airnold <jsyoo@bistel.com>
2017-06-26 11:29:05 +0900

    feat(drag) : drag & selection box complete

--- src/app/common/plugin/drag-selector/drag-base.ts
+++ src/app/common/plugin/drag-selector/drag-base.ts
@@ -34,14 +34,30 @@ export class DragBase {
     private _addEvent(target) {
         const mouseDowns = Observable.fromEvent(target[0][0], 'mousedown');
         const mouseUps = Observable.fromEvent(target[0][0], 'mouseup');
-        const mouseMoves = Observable.fromEvent(target[0][0], 'mousemove');
+        const mouseMoves = Observable.fromEvent<MouseEvent>(target[0][0], 'mousemove');
         let dragStart: Observable<any>;
 
-        dragStart = mouseDowns.debounceTime(2000);
+        mouseDowns.subscribe(() => {
+            target.select('.selection_box').remove();
+        });
+
+        dragStart = mouseDowns.flatMap(() =>
+            mouseMoves
+                .filter(x => x.movementX !== 0 || x.movementY !== 0)
+                .takeUntil(mouseUps)
+                .take(1)
+        );
         dragStart.subscribe( (e: any) => {
             this.offsetX = e.offsetX + 1;
             this.offsetY = e.offsetY + 1;
-            console.log( 'dragstart x: ', this.offsetX, 'y:', this.offsetY );
+            target.append( 'rect')
+                .attr('class', 'selection_box')
+                .attr('x', this.offsetX)
+                .attr('y', this.offsetY)
+                .attr('width', 0)
+                .attr('height', 0)
+                .style('fill', 'grey')
+                .style('fill-opacity', 0.5);
         });
 
         dragStart.map(function () {
@@ -49,9 +65,16 @@ export class DragBase {
         })
         .concatAll()
         .subscribe( (e: any) => {
-            this.moveX = e.x - this.offsetX;
-            this.moveY = e.y - this.offsetY;
+            this.moveX = e.offsetX - this.offsetX;
+            this.moveY = e.offsetY - this.offsetY;
+
             // move가 되는 동안에 d3 rect를 그려준다.
+            const s_box: any = target.select('.selection_box');
+            if ( !s_box.empty()) {
+                s_box.attr('width', this.moveX);
+                s_box.attr('height', this.moveY);
+            }
+
         });
 
         mouseUps.subscribe( (e: any) => {
@@ -61,7 +84,6 @@ export class DragBase {
             dispatchEvent(new CustomEvent(DragBase.DRAG_END, {
                 detail: dragable
             }));
-            console.log( 'mouseUps x: ', this.moveX, 'y:', this.moveY );
         });
     }
 }
