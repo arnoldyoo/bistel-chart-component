ac1badb7d728b3ea5d4616051b7e7547beaac84a
Kang HyoSeon <hskang1@bistel.co.kr>
2017-06-26 11:31:14 +0900

    feat(event dispatcher) : add event dispatcher

--- src/app/common/event/event-dispatcher.ts
+++ src/app/common/event/event-dispatcher.ts
@@ -1,5 +1,12 @@
+import { ChartEventData } from './chart-event.interface';
+
+interface EventMap { // indexable interface
+    [type: string]: any;
+}
+
 export class EventDispatcher {
     private static Instance: EventDispatcher = null;
+    private _eventMap: EventMap;
 
     public static getInstance(): EventDispatcher {
         if (EventDispatcher.Instance === null) {
@@ -8,15 +15,17 @@ export class EventDispatcher {
         return EventDispatcher.Instance;
     }
 
+    public addEventListener(type: string, method: any) {
+        this._eventMap[type] = method;
+    }
+
+    public dispatchEvent(type: string, data: ChartEventData) {
+        if (this._eventMap[type]) {
+            this._eventMap[type](data);
+        }
+    }
+
     private constructor() {
-
-    }
-
-    addEventListner() {
-
-    }
-
-    dispatchEvent() {
-
+        this._eventMap = {};
     }
 }
